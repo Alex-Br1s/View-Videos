86edd903a7aee4998b0efc1e63df1477bfc6dc2a
Alexander Bris <alexbris21@gmail.com>
2024-03-18 23:47:49 -0300

    prueba

--- src/api/searchVideos.ts
+++ src/api/searchVideos.ts
@@ -4,7 +4,7 @@
 const apiKey = import.meta.env.VITE_API_KEY_YT;
 async function searchVideos(searchTerm: string) {
     const encodedSearchTerm = encodeURIComponent(searchTerm); // Codificar el término de búsqueda
-    const response = await fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=52&q=${encodedSearchTerm}&key=${apiKey}`)
+    const response = await fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=100&q=${encodedSearchTerm}&key=${apiKey}`)
     const data = await response.json();
     return data.items
 }
--- src/api/videosMostPopular.ts
+++ src/api/videosMostPopular.ts
@@ -2,7 +2,7 @@
 
 const apiKey = import.meta.env.VITE_API_KEY_YT;
 async function videoMostPopular () {
-    const response = await fetch(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=52&regionCode=MX&key=${apiKey}`)
+    const response = await fetch(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=100&regionCode=MX&key=${apiKey}`)
     const data = await response.json();
     return data.items;
 }
