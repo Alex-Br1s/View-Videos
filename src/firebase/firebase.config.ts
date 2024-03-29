570082b308c6693adb07722e264f9aa87209e2e0
Alexander Bris <alexbris21@gmail.com>
2024-03-29 13:33:31 -0300

    configuracion nueva de firebase

--- src/firebase/firebase.config.ts
+++ src/firebase/firebase.config.ts
@@ -4,9 +4,9 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY_FIREBASE,
-  authDomain: "view-videos-1e914.firebaseapp.com",
-  projectId: "view-videos-1e914",
-  storageBucket: "view-videos-1e914.appspot.com",
+  authDomain: "view-videos2.firebaseapp.com",
+  projectId: "view-videos2",
+  storageBucket: "view-videos2.appspot.com",
   messagingSenderId: import.meta.env.VITE_MESSAGE_FIREBASE,
   appId: import.meta.env.VITE_APPID
 };
@@ -14,3 +14,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app)
+
+
+//Nombre publico del projecto: project-765703907237
